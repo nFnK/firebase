670189a7f8f98eda5ea13e82260613cf5e0b48b8
betojsc <beto_jsc@hotmail.com>
2018-06-08 11:19:08 -0500

    Initial commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+ 
+// Firebase API Key
+define('FIREBASE_API_KEY', 'AAAA4AFkkEg:APA91bGECOw2N_6cIc3ZiwxgvrZDb1Wty_R3HBMHq76lBeScMZM2HnwEuo4Vf1-T4Amo9LbFLJ11_HIso64TeesX07hH8h_Q9T_ISyX6xshCQd5mWWVimQXhQIVr2CNfstRqfQmwKv3c');
